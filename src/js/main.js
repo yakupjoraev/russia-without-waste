865756292c148083c75d8bda187a87d778401ca4
Yakup <yakupjoraev@gmail.com>
2023-12-20 15:40:12 +0300

    added new section

--- src/js/main.js
+++ src/js/main.js
@@ -1,23 +1,28 @@
 // Custom scripts
 document.addEventListener("DOMContentLoaded", function () {
-  const line1 = document.querySelector('[data-line-1]');
-  const line2 = document.querySelector('[data-line-2]');
+  const elementsWithParallax = document.querySelectorAll('[data-parallax]');
 
   // Параллакс при движении мыши
   document.addEventListener('mousemove', function (e) {
     const offsetX = e.clientX / window.innerWidth - 0.5;
     const offsetY = e.clientY / window.innerHeight - 0.5;
-    moveLines(offsetX * 20, offsetY * 20);
+    moveElements(elementsWithParallax, offsetX * 20, offsetY * 20);
   });
 
   // Параллакс при прокрутке
   document.addEventListener('scroll', function () {
     const scrollTop = window.scrollY / window.innerHeight;
-    moveLines(0, scrollTop * 20);
+    moveElements(elementsWithParallax, 0, scrollTop * 20);
   });
 
-  function moveLines(offsetX, offsetY) {
-    line1.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
-    line2.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+  function moveElements(elements, offsetX, offsetY) {
+    elements.forEach(element => {
+      const shouldApplyParallax = element.dataset.parallax === "true";
+      if (shouldApplyParallax) {
+        element.style.transform = `translate(${offsetX}px, ${offsetY}px)`;
+      } else {
+        element.style.transform = 'none';
+      }
+    });
   }
 });
